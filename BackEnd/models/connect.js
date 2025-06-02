830db8b2f27f19396675b8ac1b7e0880d5c3633e
Rodrigo D <neonrok@pm.me>
2025-06-02 12:36:26 +0100

    a relação funciona

--- BackEnd/models/connect.js
+++ BackEnd/models/connect.js
@@ -36,11 +36,10 @@ bd.Resposta = require("./resp.model.js")(sequelize, Sequelize.DataTypes);
 //bd.Reuniao = require("./reuniao.model.js")(sequelize, Sequelize.DataTypes);
 
 
-/*
 //defenir relações
-bd.Tipo_Cargos.hasMany(bd.Perfil, {foreignKey: 'id_cargo', onDelete: 'RESTRICT', allowNull: false})
-bd.Perfil.belongsTo(bd.Tipo_Cargos, {foreignKey: 'cargo_id', as: 'cargo', onDelete: 'RESTRICT', allowNull: false})
-*/
+bd.Tipo_Cargos.hasMany(bd.Perfil, {foreignKey: 'id_cargo', onDelete: 'RESTRICT', allowNull: false});
+bd.Perfil.belongsTo(bd.Tipo_Cargos, {foreignKey: 'cargo_id', as: 'cargo', onDelete: 'RESTRICT', allowNull: false});
+
 (async () => {
     try {
         await bd.sequelize.sync({ alter: true });
